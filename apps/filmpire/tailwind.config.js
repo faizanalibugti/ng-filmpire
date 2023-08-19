4d030697940e6c7fa548bb371d54bbe25a436439
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-08-19 08:33:10 +0500

    style: Tailwind Config & Material Theming

--- apps/filmpire/tailwind.config.js
+++ apps/filmpire/tailwind.config.js
@@ -3,9 +3,13 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
+  important: true,
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
+    'libs/**/*.{html,ts,css,scss}',
   ],
   theme: {
     extend: {},
