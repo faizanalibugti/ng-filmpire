0b2dd6fb37cbc581dc9ee51348d94f993c6d67c7
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-08-19 08:36:19 +0500

    feat: app routes

--- apps/filmpire/src/app/app.routes.ts
+++ apps/filmpire/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Route } from '@angular/router';
+import {
+  ActorsComponent,
+  MovieInformationComponent,
+  MoviesComponent,
+  ProfileComponent,
+} from '@ng-filmpire/ui';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'movie/:id',
+    component: MovieInformationComponent,
+  },
+  {
+    path: 'actor/:id',
+    component: ActorsComponent,
+  },
+  {
+    path: '',
+    component: MoviesComponent,
+  },
+  {
+    path: 'profile/:id',
+    component: ProfileComponent,
+  },
+];
