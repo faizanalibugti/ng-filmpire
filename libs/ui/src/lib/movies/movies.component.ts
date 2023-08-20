b62db2852886e7e9236f3277ac8504963635d47b
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-08-20 17:54:00 +0500

    feat: rendering movies for genre based on route params

--- libs/ui/src/lib/movies/movies.component.ts
+++ libs/ui/src/lib/movies/movies.component.ts
@@ -1,26 +1,50 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
+import { ActivatedRoute, Params } from '@angular/router';
 import { BaseEntity, Movie } from '@ng-filmpire/api-interfaces';
 import { MovieHttpService } from '@ng-filmpire/core-data';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'ng-filmpire-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss'],
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
   @ViewChild('topOfPage') topOfPage!: ElementRef;
 
-  moviesList$: Observable<BaseEntity<Movie>> =
-    this.movieHttp.loadPopularMovies();
+  categoryOrGenreChanged$!: Observable<Params>;
+  moviesList$: Observable<BaseEntity<Movie>> = this.movieHttp.getMovies();
+  currentGenreOrCategory!: number | string;
 
-  constructor(private movieHttp: MovieHttpService) {}
+  constructor(
+    private movieHttp: MovieHttpService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryOrGenreChanged$ = this.route.params.pipe(
+      tap(({ id }) => {
+        this.scrollToTop();
+        this.currentGenreOrCategory = Number(id) ? Number(id) : String(id);
+        this.moviesList$ = this.movieHttp.getMovies(
+          this.currentGenreOrCategory
+        );
+      })
+    );
+  }
 
   goToPage(page: PageEvent) {
-    this.moviesList$ = this.movieHttp.loadPopularMovies(page.pageIndex + 1);
+    this.moviesList$ = this.movieHttp.getMovies(
+      this.currentGenreOrCategory,
+      page.pageIndex + 1
+    );
 
-    this.topOfPage.nativeElement.scrollIntoView({
+    this.scrollToTop();
+  }
+
+  scrollToTop() {
+    this.topOfPage?.nativeElement.scrollIntoView({
       behavior: 'smooth',
       block: 'start',
     });
