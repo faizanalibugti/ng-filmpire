62f98b37f53d793a81f34dd74e2de6fcb13dee0f
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-08-20 13:20:50 +0500

    feat(ui): Movies List Component
    * rendering films via TMDB API
    * added pagination functionality

--- libs/ui/src/lib/movies/movies.component.ts
+++ libs/ui/src/lib/movies/movies.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { BaseEntity, Movie } from '@ng-filmpire/api-interfaces';
+import { MovieHttpService } from '@ng-filmpire/core-data';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ng-filmpire-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss'],
 })
-export class MoviesComponent {}
+export class MoviesComponent {
+  @ViewChild('topOfPage') topOfPage!: ElementRef;
+
+  moviesList$: Observable<BaseEntity<Movie>> =
+    this.movieHttp.loadPopularMovies();
+
+  constructor(private movieHttp: MovieHttpService) {}
+
+  goToPage(page: PageEvent) {
+    this.moviesList$ = this.movieHttp.loadPopularMovies(page.pageIndex + 1);
+
+    this.topOfPage.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -6,11 +6,11 @@ import { MoviesComponent } from './movies/movies.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { MaterialModule } from '@ng-filmpire/material';
-import { ReactiveFormsModule } from '@angular/forms';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, MaterialModule],
+  imports: [CommonModule, ReactiveFormsModule, MaterialModule, RouterModule],
   declarations: [
     ActorsComponent,
     MovieInformationComponent,
