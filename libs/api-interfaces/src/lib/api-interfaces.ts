a2c878e955aae4579e1115d0de74c011ee35481b
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-08-20 13:22:08 +0500

    fix: interfaces file renamed

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,1 +1,2 @@
-export * from './movie/movie.interface';
+export * from './base-entity/base-entity.model';
+export * from './movie/movie.model';
---
+++ libs/api-interfaces/src/lib/base-entity/base-entity.model.ts
@@ -0,0 +1,6 @@
+export interface BaseEntity<T> {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
