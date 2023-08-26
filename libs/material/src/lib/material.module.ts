f56faba91964877d260dcc04390f3308fd2ccddc
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-08-26 15:04:04 +0500

    fix(material): ButtonToggle & AutoComplete
    * material module updated

--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -36,6 +38,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatListModule,
     MatCardModule,
     MatPaginatorModule,
+    MatButtonToggleModule,
+    MatAutocompleteModule,
   ],
 })
 export class MaterialModule {}
