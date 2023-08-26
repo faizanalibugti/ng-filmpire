5747e2ed64aef319834a1cdb7f7672e1e501a46b
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-08-26 17:15:21 +0500

    feat(ui): Sidebar
    * Sidebar maintains a list of categories & genres for Movies & TV Shows
    * added attribution to TMDB
    * Styling modified to ensure only genre list is scrollable while other elements remain fixed

--- libs/ui/src/lib/sidebar/sidebar.component.ts
+++ libs/ui/src/lib/sidebar/sidebar.component.ts
@@ -1,30 +1,86 @@
-import { Component, Input } from '@angular/core';
-import { Genre } from '@ng-filmpire/api-interfaces';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state,
+} from '@angular/animations';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { Category, Genre, SelectedMedia } from '@ng-filmpire/api-interfaces';
 
 @Component({
   selector: 'ng-filmpire-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(250),
+      ]),
+      transition('* => void', [
+        animate(250, style({ transform: 'translateX(100%)' })),
+      ]),
+    ]),
+  ],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnChanges {
   @Input() darkMode!: boolean;
+  @Input() currentMedia!: SelectedMedia;
   @Input() genres!: Genre[];
 
-  categories = [
-    {
-      id: 1,
-      name: 'popular',
-      value: 'popular',
-    },
-    {
-      id: 2,
-      name: 'top rated',
-      value: 'top_rated',
-    },
-    {
-      id: 3,
-      name: 'upcoming',
-      value: 'upcoming',
-    },
-  ];
+  @Output() mediaSelected = new EventEmitter<SelectedMedia>();
+
+  categories!: Category[];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes && changes['currentMedia']) {
+      this.setCategories();
+    }
+  }
+
+  setCategories() {
+    this.categories = [
+      {
+        id: 1,
+        name: 'popular',
+        value: 'popular',
+      },
+      {
+        id: 2,
+        name: 'trending',
+        value: 'trending',
+      },
+      {
+        id: 3,
+        name: 'top rated',
+        value: 'top_rated',
+      },
+      this.currentMedia === 'movie'
+        ? {
+            id: 4,
+            name: 'upcoming',
+            value: 'upcoming',
+          }
+        : {
+            id: 4,
+            name: 'On the Air',
+            value: 'on_the_air',
+          },
+    ];
+  }
+
+  getMediaSelection(media: MatButtonToggleChange) {
+    this.currentMedia = media.value;
+    this.mediaSelected.emit(media.value as SelectedMedia);
+  }
 }
