172e8875463a94c6b4573c56723e207f606ae8b5
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-09-09 18:13:09 +0500

    revert: Routing Components removed from UI lib
    * routing components to be shifted to filmpire app
    * CI pipeline modified

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,6 +1,1 @@
-export * from './lib/actors/actors.component';
-export * from './lib/movie-information/movie-information.component';
-export * from './lib/movies/movies.component';
-export * from './lib/navbar/navbar.component';
-export * from './lib/profile/profile.component';
 export * from './lib/ui.module';
