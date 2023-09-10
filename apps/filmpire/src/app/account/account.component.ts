e42fc903649f20581c9193f6bf0582586ffc41b0
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-09-10 15:02:37 +0500

    feat: Account Page
    * component generated for routing purpose at present

---
+++ apps/filmpire/src/app/account/account.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-filmpire-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss'],
+})
+export class AccountComponent {}
