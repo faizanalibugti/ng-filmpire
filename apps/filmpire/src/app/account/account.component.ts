8a0b67458b718e0a14050e67977d78a962d5452f
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-10-22 13:31:41 +0500

    feat(app): User Profile Component
    * allows user to view their favorite & watchlisted movies & TV shows
    * the component is accessible after the user logs in
    
    TODO - implement Angular gaurd for this component

--- apps/filmpire/src/app/account/account.component.ts
+++ apps/filmpire/src/app/account/account.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfileData } from '@ng-filmpire/api-interfaces';
+import { AccountService, AuthService } from '@ng-filmpire/core-data';
+import { Observable, filter, map, concatMap, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'ng-filmpire-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss'],
 })
-export class AccountComponent {}
+export class AccountComponent implements OnInit {
+  profileData$!: Observable<ProfileData>;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private accountHttp: AccountService,
+    private auth: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.profileData$ = this.route.params.pipe(
+      filter(({ id }) => !!id),
+      map(({ id }) => Number(id)),
+      concatMap((id) =>
+        forkJoin({
+          favoriteMovies: this.accountHttp.getFavoriteMovies(id),
+          favoriteTV: this.accountHttp.getFavouriteTV(id),
+          watchListMovies: this.accountHttp.getWatchListMovies(id),
+          watchListTV: this.accountHttp.getWatchListTV(id),
+        })
+      )
+    );
+  }
+
+  logout() {
+    this.auth.logOut();
+    this.router.navigate(['/']);
+  }
+}
