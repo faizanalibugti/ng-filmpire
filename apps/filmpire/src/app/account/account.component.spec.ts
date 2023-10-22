2261ae9ca1861500af8dff30f4037c321e671aa7
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-10-22 13:50:39 +0500

    ci: prettier formatting

--- apps/filmpire/src/app/account/account.component.spec.ts
+++ apps/filmpire/src/app/account/account.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountComponent } from './account.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
@@ -8,6 +10,7 @@ describe('AccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [AccountComponent],
     }).compileComponents();
 
