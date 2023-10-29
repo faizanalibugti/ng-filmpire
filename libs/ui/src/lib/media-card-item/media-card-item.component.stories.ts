890308e42bbfae4122410038a45db1c72b57425a
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-10-29 14:32:20 +0500

    storybook: Media Card Item
    * documented media card item component states in Storybook

---
+++ libs/ui/src/lib/media-card-item/media-card-item.component.stories.ts
@@ -0,0 +1,77 @@
+import { CoreDataModule } from '@ng-filmpire/core-data';
+import {
+  Meta,
+  componentWrapperDecorator,
+  moduleMetadata,
+} from '@storybook/angular';
+import { MediaCardItemComponent } from './media-card-item.component';
+import { MaterialModule } from '@ng-filmpire/material';
+
+export default {
+  title: 'MediaCardItem',
+  component: MediaCardItemComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [CoreDataModule, MaterialModule],
+    }),
+    componentWrapperDecorator(
+      (story) => `<div class="grid grid-cols-card">${story}</div>`
+    ),
+  ],
+} as Meta<MediaCardItemComponent>;
+
+export const Movie = {
+  args: {
+    media: {
+      original_title: 'Mission: Impossible - Dead Reckoning Part One',
+      overview:
+        "Ethan Hunt and his IMF team embark on their most dangerous mission yet: To track down a terrifying new weapon that threatens all of humanity before it falls into the wrong hands. With control of the future and the world's fate at stake and dark forces from Ethan's past closing in, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy, Ethan must consider that nothing can matter more than his mission—not even the lives of those he cares about most.",
+      poster_path: '/NNxYkU70HPurnNCSiCjYAmacwm.jpg',
+      release_date: '2023-07-08',
+      title: 'Mission: Impossible - Dead Reckoning Part One',
+      vote_average: 7.727,
+    },
+  },
+};
+
+export const TV = {
+  args: {
+    media: {
+      name: 'Loki',
+      original_language: 'en',
+      original_name: 'Loki',
+      first_air_date: '2021-06-09',
+      poster_path: '/voHUmluYmKyleFkTu3lOXQG702u.jpg',
+      media_type: 'tv',
+      vote_average: 8.164,
+    },
+  },
+};
+
+export const image_loading = {
+  args: {
+    media: {
+      name: 'Loki',
+      original_name: 'Loki',
+      poster_path: 'image',
+      media_type: 'tv',
+      first_air_date: '2021-06-09',
+      vote_average: 8.164,
+    },
+    imageLoading: true,
+  },
+};
+
+export const fallback_image = {
+  args: {
+    media: {
+      name: 'Loki',
+      original_name: 'Loki',
+      poster_path: '',
+      media_type: 'tv',
+      first_air_date: '2021-06-09',
+      vote_average: 8.164,
+    },
+    imageLoading: false,
+  },
+};
